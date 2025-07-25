fdacf3141c393cbb3573a63cd485f1ddf68a3153
Amie Corso <amie.corso@coinbase.com>
2025-07-25 10:19:56 -0700

    add some notes and cleanup

--- src/CashbackOperator.sol
+++ src/CashbackOperator.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.23;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -6,18 +6,59 @@ import {AuthCaptureEscrow} from "commerce-payments/AuthCaptureEscrow.sol";
 import {Flywheel} from "./Flywheel.sol";
 import {SimpleRewards} from "./hooks/SimpleRewards.sol";
 
+/**
+ * NOTE:
+ * - Most unfortunate realization in this PoC is the need to track per-payment allocations for post-reclaim cleanups bc flywheel aggregates allocations per address (payer)
+ * - next-step functionality might be to investigate sourcing liquidity for cashback from elsewhere (e.g. a partner):
+ *     - Regardless of the implementation there's an inherent complexity to this concept given that cashback liquidity may need to be returned to
+ *         the provider in some cases:
+ *             - i.e. if the payment is voided, payment is reclaimed and therefore cleaned up, or during refund scenarios, etc.
+ *     - There is a straightforward way to handle this if we're willing to track the source of the liquidity onchain, but this would potentially
+ *         add a lot of storage writes and inefficienty to the contract (especially if liquidity were sourced from multiple parties for one payment)
+ *
+ *     - Implementation-wise, this could be supported via the onAllocate and onDistribute hooks in the rewards hook that would essentially use token collectors
+ *         for the campaign, similarly to how the AuthCaptureEscrow gathers liquidity from the payer.
+ *         - This would require different/new interfaces on this contract for passing the necessary hookdata (like signature based auths)
+ *
+ *     - Some additional notes on the refund scenario for third-party liquidity sourcing:
+ *         - 0) let's assume the cashback liquidity was provided by a "partner" ($1) on a payment of $100
+ *         - 1) the refund is provided by "shopify" and they refund the payment amount minus cashback amount (i.e. $99 goes back to buyer, buyer is whole at $100)
+ *         - 2) the merchant still has the full $100 from the original payment, so it gives this (offchain?) to the refunder, shopify
+ *         - 3) the refunder now has one extra dollar from what they paid for the refund ($100 in pocket v.s. $99 they actually refunded)
+ *             this extra dollar should really go back to the partner who sourced it in the first place.
+ *          - A more ideal flow would be for Shopify to refund the full $100, $99 of which goes to the buyer and $1 goes back to the partner.
+ *             (but as discussed above, this requires tracking state onchain)
+ *     - Perhaps thorough event coverage is enough and refunding cashback providers simply happens offchain based on events?
+ *         Would be cool to at least design things in a way where this (tracking) functionality could be extensible. Which I suppose it is since we can always design
+ *         new campaign hooks (which might store state about the cashback provider and how to refund them), as well as new campaign token collectors, which might
+ *         be able to provide/write the necessary information at the time cashback liquidity is sourced.
+ * - Totally different approach to sourcing liquidity from different sources would be to have the operator simply batch/bundle necessary calls to get the
+ *     cashback liquidity into the campaign during the allocation phase.
+ *
+ * TODO: This is now where a different token collector for refunds becomes useful/important so that we don't have to store operator liquidity for refunds
+ * in this CashbackOperator contract. We'd want to be able to use signature based auths to source liquidity from a different address.
+ *
+ *
+ */
 contract CashbackOperator is Ownable {
     uint256 public constant MAX_CASHBACK_BPS = 10000;
 
     /// @notice Cashback percentage in basis points
     uint256 public cashbackBps;
+
+    /// @notice The escrow contract
     AuthCaptureEscrow public escrow;
+
+    /// @notice The flywheel contract
     Flywheel public flywheel;
+
+    /// @notice The rewards hook contract
     SimpleRewards public rewardsHook;
 
+    /// @notice The cashback campaign
     address public cashbackCampaign;
 
-    /// @notice Tracks the refunded amount for each payment
+    /// @notice Tracks the refunded amount for each payment (for refunding)
     mapping(bytes32 paymentInfoHash => uint256 amountRefunded) public amountRefunded;
 
     /// @notice Tracks allocated cashback amount per payment
@@ -131,6 +172,28 @@ contract CashbackOperator is Ownable {
         _deallocateCashback(paymentInfo, paymentState.capturableAmount);
     }
 
+    /// @notice Return previously-captured tokens to payer, minus the original cashback amount
+    /// @dev Can be called by operator
+    /// @dev Funds are transferred from the caller or from the escrow if token collector retrieves external liquidity
+    /// @param paymentInfo PaymentInfo struct
+    /// @param amount Amount to refund
+    /// @param tokenCollector Address of the token collector
+    /// @param collectorData Data to pass to the token collector
+    function refund(
+        AuthCaptureEscrow.PaymentInfo calldata paymentInfo,
+        uint256 amount,
+        address tokenCollector,
+        bytes calldata collectorData
+    ) external {
+        // compute the amount of the refund that was cashback in the first place
+        uint256 originalCashbackAmount = (amount * cashbackBps) / MAX_CASHBACK_BPS;
+        // deduct the cashback amount from the amount refunded
+        amount -= originalCashbackAmount;
+        escrow.refund(paymentInfo, amount, tokenCollector, collectorData);
+        // Shouldn't be anything to allocate or distribute here
+        // TODO: EVENTS
+    }
+
     /// @notice Cleans up orphaned allocation after a payment was reclaimed directly from AuthCaptureEscrow
     /// @dev Can be called by anyone to clean up orphaned allocations
     /// @param paymentInfo PaymentInfo struct for the reclaimed payment
@@ -157,28 +220,6 @@ contract CashbackOperator is Ownable {
             // Clear the allocation tracking
             delete paymentAllocations[paymentHash];
         }
-    }
-
-    /// @notice Return previously-captured tokens to payer, minus the original cashback amount
-    /// @dev Can be called by operator
-    /// @dev Funds are transferred from the caller or from the escrow if token collector retrieves external liquidity
-    /// @param paymentInfo PaymentInfo struct
-    /// @param amount Amount to refund
-    /// @param tokenCollector Address of the token collector
-    /// @param collectorData Data to pass to the token collector
-    function refund(
-        AuthCaptureEscrow.PaymentInfo calldata paymentInfo,
-        uint256 amount,
-        address tokenCollector,
-        bytes calldata collectorData
-    ) external {
-        // compute the amount of the refund that was cashback in the first place
-        uint256 originalCashbackAmount = (amount * cashbackBps) / MAX_CASHBACK_BPS;
-        // deduct the cashback amount from the amount refunded
-        amount -= originalCashbackAmount;
-        escrow.refund(paymentInfo, amount, tokenCollector, collectorData);
-        // Shouldn't be anything to allocate or distribute here
-        // TODO: EVENTS
     }
 
     /// @notice Gets the payment state for a payment
@@ -255,7 +296,7 @@ contract CashbackOperator is Ownable {
         flywheel.deallocate(cashbackCampaign, paymentInfo.token, hookData);
 
         // Clear allocation tracking since it's now deallocated
-        // TODO: should this just be deleted? (does it always go to zero in the only case where it's called?)
+        // TODO: should this just be deleted? (thinks this always goes to zero in the only case where it's called?)
         paymentAllocations[paymentHash] -= cashbackAmount;
     }
 }
