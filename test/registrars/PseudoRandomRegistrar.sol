96c631d1a8b244c30c04de6bc280a4a0de38b6dc
Conner Swenberg <conner.swenberg@coinbase.com>
2025-09-13 13:09:50 -0700

    Add PseudoRandomRegistrar test skeletons

---
+++ test/registrars/PseudoRandomRegistrar.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import {BuilderCodesBase} from "../BuilderCodes/BuilderCodesBase.sol";
+
+import {PseudoRandomRegistrar} from "../../src/registrars/PseudoRandomRegistrar.sol";
+
+/// @notice Tests for PseudoRandomRegistrar
+contract PseudoRandomRegistrarTest is BuilderCodesBase {
+    PseudoRandomRegistrar pseudoRandomRegistrar;
+
+    function setUp() public override {
+        super().setUp();
+
+        pseudoRandomRegistrar = new PseudoRandomRegistrar(address(builderCodes));
+    }
+
+    /**
+     * register reverts
+     */
+    function test_register_revert_zeroPayoutAddress() public {}
+
+    /**
+     * register success conditions
+     */
+    function test_register_success_setSenderCodeOwner(address payoutAddress) public {}
+
+    function test_register_success_setPayoutAddress(address payoutAddress) public {}
+
+    function test_register_success_codePrefixed(address payoutAddress) public {}
+
+    function test_register_success_codeSuffixAlphanumeric(address payoutAddress) public {}
+
+    function test_register_success_codeSuffixFixedLength(address payoutAddress) public {}
+
+    function test_register_success_nonceChanged(address payoutAddress) public {}
+
+    function test_register_success_repeatedRegistrationsDiffer(address payoutAddress) public {}
+}
